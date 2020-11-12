d7d268114f27a75d28ba4f4aab3a32d1cb7d084e
witampanstwa <robert.dziubek5@gmail.com>
2020-11-12 19:09:55 +0100

    Match parsing fixup

--- scripts/styler.js
+++ scripts/styler.js
@@ -3,8 +3,13 @@ class Styler {
     static Verbose = class {
 
         static colourTagBoundaries(content, colourClass) {
-            return content.replace(MatchExpressions.STARTING_TAG,
-                `$1${colourClass}$2${Syntactics.VERBOSE_STYLE_CLASS_CLOSING_TAG}$3`);
+            return content
+                .replace(MatchExpressions.STARTING_TAG,
+                    `$1${colourClass}$2${Syntactics.VERBOSE_STYLE_CLASS_CLOSING_TAG}$3`)
+                .replace(MatchExpressions.ENDING_TAG, (_, remaining, tagName, bracket) =>
+                    `${remaining}${tagName === null
+                        ? tagName : `${colourClass}${tagName}${Syntactics.VERBOSE_STYLE_CLASS_CLOSING_TAG}`
+                    }${bracket}`);
         }
     }
 }
