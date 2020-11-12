d7563511ffeb7fd07a6ada1b0bdf3c875886501d
witampanstwa <robert.dziubek5@gmail.com>
2020-11-12 19:08:45 +0100

    Regex greediness, optionality fixup

--- scripts/enum/match-expressions.js
+++ scripts/enum/match-expressions.js
@@ -1,5 +1,5 @@
 const MatchExpressions = {
-    STARTING_TAG: /(^<)(\w+)(.*)/,
-    ENDING_TAG: /(.*)(\w+)(>$)/,
+    STARTING_TAG: /(<)(\w+)(.*)/,
+    ENDING_TAG: /(.*?)(\w+)(>$)/,
     TAG_UNCLOSED: ``,
 }
