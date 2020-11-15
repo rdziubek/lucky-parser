97408caf4f386ff7b448e9df25fe07009680fdd5
witampanstwa <robert.dziubek5@gmail.com>
2020-11-15 21:29:33 +0100

    RegEx bugfix

--- scripts/enum/match-expressions.js
+++ scripts/enum/match-expressions.js
@@ -12,7 +12,11 @@ const MatchExpressions = {
     TAG_UNCLOSED: ``,
     TAG_SCRAMBLE_ABSTRACT: /<\s*(\w+)(.*?)>/g,
     TAG_SCRAMBLE_ASSOCIATIVE_PROPERTIES: /(\w+)=(\w+)/g,
-    TAG_SCRAMBLE_LOOSE_PROPERTIES: /\s+((?:".*?")|(?:[^\s]+))/g,
+    TAG_SCRAMBLE_PROPERTIES: /\s+((?:".*?")|(?:[^\s]+))/g,
+    TAG_SCRAMBLE_CURLY_BRACKETS: /.*?({.*}).*?/g,
+    TAG_SCRAMBLE_BRACKETS_CONTENT: /{\s*([^}{]+)\s*}/g,
+    TAG_SCRAMBLE_CLASSES_FOLLOWING_CURLY_BRACKETS: /.*}\s*(.*?)/g,
+    TAG_SCRAMBLE_CLASSES_CONTENT: /\.[A-Za-z0-9_-]/g,
     ALL_UNTIL_NEXT_SCOPE: {
         VERBOSE: /(.*)&lt;-$/s,
         RENDERED: /(.*)<-$/s,
